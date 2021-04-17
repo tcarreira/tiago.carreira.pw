14a04b4ecd9b4a637131efae9203f20a9346881b
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2021-04-17 11:42:28 -0300

    refactor mail shadowing js

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,13 @@
 // trying to avoid spam
 document.addEventListener('DOMContentLoaded', function() {
-    maildiv = document.getElementById("shadowed-mail")
-    if (maildiv !== null) {
+    maildivs = document.getElementsByClassName("shadowed-mail")
+    for (let maildiv of maildivs) {
         direction = maildiv.getAttribute("direction")
         domain = maildiv.getAttribute("domain")
         subject = maildiv.getAttribute("subject")
+        original = maildiv.getAttribute("original")
         fullm = direction+"@"+domain
         maildiv.href = "mailto:"+fullm+"?subject="+subject
-    }
+        maildiv.innerHTML = maildiv.innerHTML.replaceAll(original, fullm)
+    };
 });
