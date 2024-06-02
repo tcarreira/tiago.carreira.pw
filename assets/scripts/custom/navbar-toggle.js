dd3b2547f31f3b6b4ff14f7ffb456805a5494bd0
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2024-06-02 02:41:03 +0100

    prevent changing theme to scroll back home

--- assets/scripts/custom/navbar-toggle.js
+++ assets/scripts/custom/navbar-toggle.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // auto-toggle navbar on items click
+    // alternative: https://stackoverflow.com/questions/14248194/close-responsive-navbar-automatically/75808063#75808063
     const navLinks = document.querySelectorAll('.nav-link:not(.dropdown-toggle)')
     const menuToggle = document.getElementById('navbar-toggler')
     navLinks.forEach((l) => {
@@ -6,4 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
             menuToggle.checkVisibility() && menuToggle.click()
         })
     })
+
+    //prevent changing theme to scroll back home
+    const navThemeLinks = document.querySelectorAll('#themeMenu > .nav-link')
+    navThemeLinks.forEach((l) => {
+        l.setAttribute("href", "javascript:;")
+    })
+
 });
+
